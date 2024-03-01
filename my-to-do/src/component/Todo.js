d705595f5a25f247005c6ad1bd6c2950f2ed5c2e
dj-1995sompura <sompuradigvijay999@gmail.com>
2024-03-01 22:57:46 +0530

    basic functionality done

--- my-to-do/src/component/Todo.js
+++ my-to-do/src/component/Todo.js
@@ -1,23 +1,41 @@
 import './todo.css';
 import {useState} from 'react';
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 function Todo() {
     let [input, setInput] = useState("");
-    let arr=[];
+    let [arr, setArr] = useState(
+        []
+    );
+    let [secondValue, setSecondValue] = useState(arr);
     const handleActivity = (e) =>{
+        e.stopPropagation();
         setInput(e.target.value);
         // console.log(input);
     };
 
     const handleSubmitActivity = () =>{
-        
+
+        // get task and add in array to crete a list
         let task = input;
+        if(task.length > 0){
+            setArr([...arr, task]);
+        }
 
-        arr.push(task);
-        console.log(...task,arr);
-        // console.log(task);
+        // reset value
+        setInput("");
+    }
 
-
+    const handleClear=()=>{
+        // empty arr
+        setArr([]);
+    }
+    const handleEdit =(e) =>{
+        // edit the existing activity
+        e.stopPropagation();
+        // setSecondValue(e.target.value)
+        // setInput( e.target.value);
+        // setArr(())
     }
   return (
     <div className="container">
@@ -28,10 +46,25 @@ function Todo() {
         </div>
         <div className='outputArea'>
             <ul className='activityList'>
-
+                {arr.map((item,index)=>{
+                 return(   
+                    <li key={index}>
+                        <span>{index + 1}</span>
+                        <span>{item}</span>
+                        <span>
+                            <input type='text' value={secondValue} onChange={handleEdit}/>
+                            <i className="fa fa-check" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                        <span className='edit'><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span>
+                        <span className='remove'><i class="fa fa-trash" aria-hidden="true"></i></span>
+                        </p>
+                    </li>
+                 )
+            })}
             </ul>
         </div>
-        <button className='clearData'>Clear Data</button>
+        <button className='clearData' onClick={handleClear}>Clear Data</button>
     </div>
   )
 }
